d9bc1da8c62c637706d5e1897be1b9882e6a4b33
Ruan Figuereido <huanfiguereido@gmail.com>
2023-08-16 19:29:55 -0300

    finalização do layout mobile e finalização do menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,20 @@
-//criar a função que abre e fecha o menu
-//a partir de uma variavel number que diminui
-//ou aumenta de acordo com o clique do menu
-//servindo pra indicar qual o src da menu Icon
+const menuIcon = document.querySelector("#menu-icon");
+let menu = document.querySelector("#menu");
+let openClose=0;
+menuIcon.addEventListener("click", menuOptions);
+function menuOptions(){
+    if(openClose==0){
+        menuIcon.src="assets/img/menu-open.svg";
+        menu.style.height="0px";
+        menu.style.paddingBottom="0px";
+        openClose++;
+    }
+    else if(openClose==1){
+        menuIcon.src="assets/img/menu-close.svg";
+        menu.style.height="280px";
+        menu.style.paddingBottom="1em";
+        openClose--;
+    }
+}
+//CRIAR UMA FUNÇÃO QUE EXIBE UMA CAIXA
+//DE DIALOGO DE EMAIL ENVIADO
